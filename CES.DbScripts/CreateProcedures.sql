d54f9aae15d303f9c27c7f71d1d3fce2c545c0b7
Sumant Natkar <sumant30@gmail.com>
2020-02-23 11:14:22 +0530

    Added scripts

---
+++ CES.DbScripts/CreateProcedures.sql
@@ -0,0 +1,306 @@
+﻿USE CES
+Go
+
+CREATE PROCEDURE Approve
+	@UserId UniqueIdentifier,
+	@AppId  UniqueIdentifier
+AS
+	BEGIN TRY
+	BEGIN TRANSACTION
+		Update UserApplicationAccess
+		Set
+		Granted = 1
+		Where
+		UserId=@UserId
+		And
+		ApplicationId=@AppId;
+			
+	COMMIT;		
+	END TRY
+	BEGIN CATCH
+	 ROLLBACK;
+	 Throw;
+	END CATCH
+Go
+
+CREATE PROCEDURE AuthenticateUser
+	@Username varchar(100),
+	@Password varchar(500)
+AS
+	Select 
+	u.Id,
+	u.Username,
+	u.RefreshToken,
+	u.Role
+	From 
+	Users As u
+	Where
+	u.Username = @Username
+	And
+	u.Password = @Password;
+Go
+
+Create PROCEDURE ChangePassword
+	@Username Varchar(100),
+	@Password Varchar(100)
+AS
+	BEGIN TRY
+		BEGIN TRANSACTION
+			Update Users
+			Set Password = @Password
+			Where
+			Username = @Username;
+		COMMIT;
+		END TRY
+	BEGIN CATCH
+	 ROLLBACK;
+	 Throw;
+	END CATCH
+Go
+
+Create PROCEDURE ForgotPassword
+	@Username Varchar(100)
+AS
+	Select 
+	u.Password	
+	From	Users	As u
+	Where
+	u.Username = @Username;
+Go
+
+CREATE PROCEDURE GetAllUsers
+AS
+	Select
+	u.Id,
+	u.RefreshToken,
+	u.Username	
+	From
+	Users u
+	Where
+	u.Role = 'User';
+Go
+
+CREATE PROCEDURE GetAppRequest
+	@UserId UniqueIdentifier,
+	@AppId  UniqueIdentifier
+AS
+	Select
+	count(uaa.Id)
+	From
+	UserApplicationAccess As uaa
+	Where
+	uaa.UserId = @UserId
+	And
+	uaa.ApplicationId = @AppId;
+Go
+
+CREATE PROCEDURE GetUser
+@UserId		UniqueIdentifier
+AS
+	Select
+	u.Id,
+	u.RefreshToken,
+	u.Username	
+	From
+	Users u
+	Where
+	u.Id = @UserId;
+Go
+
+CREATE PROCEDURE GetUserAppReqDetails
+	@Username Varchar(100)
+AS
+	Select 
+	a.Name			As AppName,
+	uaa.AccessType	As AccessType
+	From	Applications			As a
+	Join	UserApplicationAccess	As uaa	On a.Id = uaa.ApplicationId
+	Join	Users					As u	On uaa.UserId = u.Id
+	Where
+	u.Username = @Username;
+Go
+
+CREATE PROCEDURE GetUserDetails
+	@Username Varchar(100)
+AS
+	Select 
+	u.Id,
+	u.Username,
+	u.RefreshToken,
+	u.Password,
+	u.Role
+	From	Users	As u
+	Where
+	u.Username = @Username;
+Go
+
+CREATE PROCEDURE GetUserId
+	@Username		Varchar(100),
+	@RefreshToken	Varchar(100)
+AS
+	Select
+	u.Id
+	From Users u
+	Where
+	u.Username = @Username
+	And
+	u.RefreshToken = @RefreshToken;
+Go
+
+CREATE PROCEDURE GetUserIdByUsername
+	@Username		Varchar(100)
+AS
+	Select 
+	u.Id
+	From
+	Users u
+	Where
+	u.Username = @Username
+Go
+
+CREATE PROCEDURE Reject
+	@UserId UniqueIdentifier,
+	@AppId  UniqueIdentifier
+AS
+	BEGIN TRY
+	BEGIN TRANSACTION
+		Update UserApplicationAccess
+		Set
+		Granted = 0
+		Where
+		UserId=@UserId
+		And
+		ApplicationId=@AppId;
+			
+	COMMIT;		
+	END TRY
+	BEGIN CATCH
+	 ROLLBACK;
+	 Throw;
+	END CATCH
+Go
+
+CREATE PROCEDURE RemoveRefreshToken
+	@UserId		UniqueIdentifier
+AS
+	BEGIN TRY
+			BEGIN TRANSACTION
+					Update Users
+					Set
+					RefreshToken = null
+					Where
+					Id=@UserId
+					;
+			
+			COMMIT;
+			
+			Select 
+			u.Id,
+			u.Username,
+			u.RefreshToken,
+			u.Role
+			From 
+			Users As u
+			Where
+			u.Id=@UserId
+	
+		END TRY
+		BEGIN CATCH
+		 ROLLBACK;
+		 Throw;
+		END CATCH
+Go
+
+CREATE PROCEDURE SaveApplication
+	@ApplicationName	Varchar(100)
+AS
+BEGIN TRY
+			BEGIN TRANSACTION
+					Declare @AppId UniqueIdentifier;
+					Set @AppId = NEWID();
+
+					Insert 
+					Into 
+					Applications
+					(
+					Id,
+					Name
+					)
+					Values(@AppId,@ApplicationName);
+			COMMIT;
+
+			Select 
+			app.Id,
+			app.Name
+			From 
+			Applications As app
+			Where
+			app.Id = @AppId;
+		END TRY
+		BEGIN CATCH
+		 ROLLBACK;
+		 Throw;
+		END CATCH
+Go
+
+CREATE PROCEDURE SaveAppRequest
+	@UserId		UniqueIdentifier,
+	@AppId		UniqueIdentifier,
+	@AccessType Varchar(10)
+AS
+	BEGIN TRY
+			BEGIN TRANSACTION
+					Insert Into 
+					UserApplicationAccess
+					(
+					 Id,
+					 ApplicationId,
+					 UserId,
+					 AccessType
+					)
+					Values
+					(
+					NewId(),
+					@AppId,
+					@UserId,
+					@AccessType
+					);
+			COMMIT;
+			
+		END TRY
+		BEGIN CATCH
+		 ROLLBACK;
+		 Throw;
+		END CATCH
+Go
+
+
+CREATE Procedure SaveRefreshToken
+	@UserID			UniqueIdentifier,		
+	@RefreshToken	varchar(100)
+	As 
+	
+		BEGIN TRY
+			BEGIN TRANSACTION
+					Update Users
+					Set
+					RefreshToken = @RefreshToken
+					Where
+					Id = @UserID;
+			COMMIT;
+			Select 
+			u.Id,
+			u.Username,
+			u.RefreshToken,
+			u.Role
+			From 
+			Users As u
+			Where
+			Id = @UserID;
+		END TRY
+		BEGIN CATCH
+		 ROLLBACK;
+		 Throw;
+		END CATCH
+Go
+
---
+++ CES.DbScripts/CreateTables.sql
@@ -0,0 +1,33 @@
+﻿USE Master
+GO
+CREATE DATABASE CES
+Go
+
+CREATE TABLE [dbo].[Users] (
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [Username]     VARCHAR (100)    NOT NULL,
+    [Password]     VARCHAR (500)    NOT NULL,
+    [RefreshToken] VARCHAR (200)    NULL,
+    [Role]         VARCHAR (20)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+Go
+
+CREATE TABLE [dbo].[Applications] (
+    [Id]   UNIQUEIDENTIFIER NOT NULL,
+    [Name] VARCHAR (100)    NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+Go
+
+CREATE TABLE [dbo].[UserApplicationAccess] (
+    [Id]            UNIQUEIDENTIFIER NOT NULL,
+    [UserId]        UNIQUEIDENTIFIER NOT NULL,
+    [ApplicationId] UNIQUEIDENTIFIER NOT NULL,
+    [AccessType]    VARCHAR (10)     NOT NULL,
+    [Granted]       BIT              NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserApplicationAccess_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [FK_UserApplicationAccess_Applications] FOREIGN KEY ([ApplicationId]) REFERENCES [dbo].[Applications] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+Go
